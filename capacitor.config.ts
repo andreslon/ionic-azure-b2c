ccdefc0d5836adfc3dc16044da059bc47ebdf12b
andreslon <andreslon@outlook.com>
2024-12-15 22:01:27 -0500

    add initial Android project structure and configuration for Ionic app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-azure-b2c',
-  webDir: 'www'
+  appId: 'co.coem.ionicazureb2c',  
+  appName: 'ionic-azure-b2c', 
+  webDir: 'www',  
+  server: {
+    androidScheme: 'https',  
+  },
 };
 
 export default config;
