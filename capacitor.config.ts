99b2ac541309d98b1f56c25f665d99ccd6031434
andreslon <andreslon@outlook.com>
2024-12-15 21:41:44 -0500

    initialize new Ionic Angular application with basic structure and configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-azure-b2c',
+  webDir: 'www'
+};
+
+export default config;
